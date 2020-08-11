b76be58332ef0e1f712b9c635d3626303f13cd9d
lucaslomba <lucasazevedolomba@gmail.com>
2020-08-11 14:45:06 -0300

    estilização e funcionamento formulario

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,48 +1,52 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: 'Categoria Inicial',
     descricao: 'Descrição inicial',
-    cor: '#000'
-  }
-  
+    cor: '#000',
+  };
+
   const [categorias, setCategorias] = useState([]);
 
   const [values, setValues] = useState(valoresIniciais);
 
-  function setValue(chave, valor){
-    
-    //Chave: Nome, Descrição, bla...
+  function setValue(chave, valor) {
+    // Chave: Nome, Descrição, bla...
     setValues({
       ...values,
       [chave]: valor,
-    })
+    });
   }
 
-  function handleChange(e){
-    //Pega valor do target do evento, que tras o valor do input
-    //setValues(e.target.value)
-    setValue(e.target.getAttribute('name'), e.target.value)
+  function handleChange(e) {
+    // Pega valor do target do evento, que tras o valor do input
+    // setValues(e.target.value)
+    setValue(e.target.getAttribute('name'), e.target.value);
   }
 
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
-      <form onSubmit={function handleSubmit(e){
+      <form onSubmit={function handleSubmit(e) {
         e.preventDefault();
         setCategorias([
-          //tres pontos siguinifica que ira pegar todos os valores ja informados e somar o que vem apos a virgula
+          // tres pontos siguinifica que ira pegar todos os valores ja informados e somar o que vem apos a virgula
           ...categorias,
-          values
+          values,
         ]);
 
-        setValues(valoresIniciais)
-      }}>
+        setValues(valoresIniciais);
+      }}
+      >
         {/* State */}
         <FormField
           label="Nome da Categoria: "
@@ -54,7 +58,7 @@ function CadastroCategoria() {
 
         <FormField
           label="Descrição:"
-          type="?????"
+          type="textarea"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
@@ -68,27 +72,24 @@ function CadastroCategoria() {
           onChange={handleChange}
         />
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
       <ul>
-        {categorias.map( (categoria, indice) => {
-          return[
-            <li key={`${categorias}${indice}`}>
-              {categoria.nome}
-            </li>
-          ]
-        })}
+        {categorias.map((categoria, indice) => [
+          <li key={`${categorias}${indice}`}>
+            {categoria.nome}
+          </li>,
+        ])}
       </ul>
-
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
